221a5dff8245480d544e0686025087a121fc1deb
carlol11 <32904202+carlol11@users.noreply.github.com>
2019-03-31 18:00:30 +0800

    Added Card prefab/scripts and can be draggable

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+	public Card card;
+
+	public Text nameText;
+	public Text descriptionText;
+
+	public Image artworkImage;
+
+	public Text defenseText;
+	public Text attackText;
+	public Text valueText;
+
+	// Use this for initialization
+	void Start () {
+		nameText.text = card.name;
+		descriptionText.text = card.description;
+
+		artworkImage.sprite = card.artwork;
+
+        defenseText.text = "Def: " + card.defense.ToString();
+		attackText.text = "Atk: " + card.attack.ToString();
+        valueText.text = card.value.ToString();
+	}
+	
+}
---
+++ Assets/Scripts/RandomCard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomCard : MonoBehaviour
+{
+
+    public Button drawButton;
+    public Text text;
+    int num;
+    [SerializeField] private Card[] cards;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        drawButton.onClick.AddListener(TaskOnClick);
+    }
+    void decreaseCard()
+    {
+
+        Debug.Log("Entered");
+        num = int.Parse(text.GetComponent<Text>().text);
+        Debug.Log(num);
+        if (num >= 0)
+        {
+            num--;
+            text.GetComponent<Text>().text = num.ToString();
+        }
+        else
+        {
+            //TODO: Put message in game that user has no more cards
+            Debug.Log("No more cards");
+        }
+    }
+    void TaskOnClick()
+    {
+        decreaseCard();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
